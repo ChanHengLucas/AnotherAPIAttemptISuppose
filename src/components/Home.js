7ad364585cf0807c5a3d2885f67a76622d01e2d2
ChanHengLucas <91720922+ChanHengLucas@users.noreply.github.com>
2024-12-10 08:34:51 -0500

    second first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+function Home() {
+    return(
+        <div className="yes">
+            <p>Yippee :D</p>
+        </div>
+    )
+}
+
+export default Home;
